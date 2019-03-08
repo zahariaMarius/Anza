90049537dcc0573c204079d6a01813d549265b05
Laurentiu Jr Marius Zaharia <zaharialorenzo@gmail.com>
2019-03-08 16:32:45 +0100

    Cambio message della email

--- User/utils/emailer.js
+++ User/utils/emailer.js
@@ -17,7 +17,7 @@ module.exports = {
       from: 'AnzaFuckCoco', // sender address
       to: user.email, // list of receivers
       subject: "Hello to Anza! Please confirm email address", // Subject line
-      text: "Confirm email address at this link: " + "localhost:3000/user/active/" + user.temporarytoken, // plain text body
+      text: "Confirm email address at this link mommafucka: " + "localhost:3000/user/active/" + user.temporarytoken, // plain text body
       html: "Confirm email address at this link: " + "<b> localhost:3000/user/active/" + user.temporarytoken + "</b>" // html body
     }
   }
